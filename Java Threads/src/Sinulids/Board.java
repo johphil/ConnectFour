48bb26a0e314ff65862fa0dded439e96e1baaee2
johphil <johphilencarnacion@gmail.com>
2019-09-23 20:36:02 +0800

    Project Folder

---
+++ Java Threads/src/Sinulids/Board.java
@@ -0,0 +1,15 @@
+package Sinulids;
+
+public class Board implements Runnable {
+
+	public Board() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
